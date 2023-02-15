2f9442c6706e71e4da66cbad7f84767670210d44
Александр <hyperpositron@gmail.com>
2023-02-16 01:00:33 +0200

    first commit in branch1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Random;
+
 public class Main {
+    private static final Employee[] EMPLOYEES = new Employee[10];
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        fillEmployees();
+        printFullInfo();
+        //EMPLOYEES[0] = new Employee("Сотрудник 1", 1, 20_000);
     }
+
+    private static void fillEmployees() {
+        Random random = new Random();
+
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            EMPLOYEES[i] = new Employee(
+                    "Сотрудник " + (i + 1),
+                    random.nextInt(6),
+                    random.nextInt(90_000)
+            );
+        }
+    }
+
+    private static void printFullInfo() {
+        for (Employee employee : EMPLOYEES) {
+            System.out.println(employee);
+        }
+    }
+
 }
