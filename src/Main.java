0d62470862658d7eaff1d2e5ec79077774aa4652
Александр <hyperpositron@gmail.com>
2023-02-16 20:20:17 +0200

    done

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,11 @@ public class Main {
     public static void main(String[] args) {
         fillEmployees();
         printFullInfo();
-        //EMPLOYEES[0] = new Employee("Сотрудник 1", 1, 20_000);
+        System.out.println("Сумма ЗП: " + totalSalaries());
+        System.out.println("Сумма с минимальной ЗП: " + findEmployeeWithMinSalary());
+        System.out.println("Сумма с максимальной ЗП: " + findEmployeeWithMaxSalary());
+        System.out.println("Сумма с средней ЗП: " + findAveragesSalary());
+        printFullNameInfo();
     }
 
     private static void fillEmployees() {
@@ -15,8 +19,8 @@ public class Main {
         for (int i = 0; i < EMPLOYEES.length; i++) {
             EMPLOYEES[i] = new Employee(
                     "Сотрудник " + (i + 1),
-                    random.nextInt(6),
-                    random.nextInt(90_000)
+                    random.nextInt(6) + 1,
+                    random.nextInt(45_000) + 45_000
             );
         }
     }
@@ -25,6 +29,48 @@ public class Main {
         for (Employee employee : EMPLOYEES) {
             System.out.println(employee);
         }
-    }
+    } //Получить список всех сотрудников со всеми имеющимися по ним данными (вывести в консоль значения всех полей (toString)).
+
+    private static int totalSalaries() {
+        int sum = 0;
+        for (Employee employee : EMPLOYEES) {
+            sum += employee.getSalary();
+        }
+        return sum;
+    } // Сумма Зарплат Сотрудников
+
+    private static Employee findEmployeeWithMinSalary() {
+        int min = Integer.MAX_VALUE;
+        Employee employee = null;
+        for (Employee emp : EMPLOYEES) {
+            if (emp.getSalary() < min) {
+                min = emp.getSalary();
+                employee = emp;
+            }
+        }
+        return employee;
+    } // Найти сотрудника с минимальной зарплатой.
+
+    private static Employee findEmployeeWithMaxSalary() {
+        int max = Integer.MIN_VALUE;
+        Employee employee = null;
+        for (Employee emp : EMPLOYEES) {
+            if (emp.getSalary() > max) {
+                max = emp.getSalary();
+                employee = emp;
+            }
+        }
+        return employee;
+    }  // Найти сотрудника с максимальной зарплатой.
+
+    private static double findAveragesSalary() {
+        return totalSalaries() / (double)EMPLOYEES.length;
+    } // Найти сотрудника с средней зарплатой.
+
+    private static void printFullNameInfo() {
+        for (Employee employee : EMPLOYEES) {
+            System.out.println(employee.getFullName());
+        }
+    } //Получить Ф. И. О. всех сотрудников (вывести в консоль).
 
 }
